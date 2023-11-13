a28dfa16ee83a528dc064c0b08dbe6ae5dc0825d
Fischer <Jessica.Fischer@it.htlhl.at>
2023-11-13 11:45:12 +0100

    bisschen Fehlerausbesserung

--- src/app/components/normalmap/normalmap.component.ts
+++ src/app/components/normalmap/normalmap.component.ts
@@ -1,17 +1,15 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {
-  AxesHelper,
-  Color, GridHelper,
+  Color,
   Mesh,
   MeshPhongMaterial,
   PerspectiveCamera,
+  PlaneGeometry,
   PointLight,
   Scene,
   TextureLoader,
   WebGLRenderer
 } from "three";
-import {ParametricGeometries} from "three/examples/jsm/geometries/ParametricGeometries";
-import PlaneGeometry = ParametricGeometries.PlaneGeometry;
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 @Component({
@@ -27,60 +25,55 @@ export class NormalmapComponent  implements AfterViewInit {
   private light!: PointLight;
   private camera!: PerspectiveCamera;
   private renderer!: WebGLRenderer;
-  private plane!: ParametricGeometries.PlaneGeometry;
+  private plane!: PlaneGeometry;
   private lastTime!: number;
-  private lightSourceSpeed: number = 10;
+  private lightSourceSpeed: number = 2;
 
   async ngAfterViewInit(): Promise<void> {
     this.scene = new Scene();
     this.scene.background = new Color(Color.NAMES.white);
 
-    this.light = new PointLight(0xffffff, 1);
-    this.light.position.set(0, 10, 10);
+    this.light = new PointLight(0xffffff, 10000);
+    this.light.position.set(0, 0, 20);
     this.scene.add(this.light);
 
-    this.camera = new PerspectiveCamera(75, this.canvasRef.nativeElement.width / this.canvasRef.nativeElement.height,
-      0.1, 1000);
-
-    this.camera.position.set(0, 30, 0);
-
-    const controls = new OrbitControls(this.camera, this.canvasRef.nativeElement)
-
-    const axisHelper = new AxesHelper(5);
-    this.scene.add(axisHelper);
-    const gridHelper = new GridHelper(20);
-    this.scene.add(gridHelper);
+    this.camera = new PerspectiveCamera(
+      75,
+      this.canvasRef.nativeElement.width / this.canvasRef.nativeElement.height,
+      0.1,
+      1000
+    );
+    this.camera.position.set(0, 0, 8);
+    const controls = new OrbitControls(this.camera, this.canvasRef.nativeElement);
 
     this.renderer = new WebGLRenderer({canvas: this.canvasRef.nativeElement});
+    //this.renderer.setSize(this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
 
     this.plane = new PlaneGeometry(10, 10, 1, 1);
 
     const material = new MeshPhongMaterial();
-    const textureLoader = new TextureLoader();
+    const textureLoader = new TextureLoader()
     material.map = await textureLoader.loadAsync('assets/texture/UntouchedNormalMap.jpg');
     material.normalMap = await textureLoader.loadAsync('assets/texture/NormalMap.png');
-    material.normalScale. set(2, 2);
+    material.normalScale.set(1, 1); // Effekt der Normalmap verstärken oder abschwächen
 
     const planeMesh = new Mesh(this.plane, material);
-    planeMesh.rotation.x = 180 * Math.PI / 180
     this.scene.add(planeMesh);
-
-    requestAnimationFrame((total) => this.animate(total));
+    requestAnimationFrame((delay) => this.render(delay));
   }
 
-  private animate(total: number) {
-    const delay = (total - this.lastTime) / 1000;
-
-    this.light.position.x += this.lightSourceSpeed * delay;
-    if (this.light.position.x > 5) {
+  private render(delay: DOMHighResTimeStamp): void {
+    const delayN = (delay - this.lastTime)/1000;
+    if(this.light.position.x >5){
       this.lightSourceSpeed *= -1;
-    } else if (this.light.position.x < -5) {
+    } else if (this.light.position.x < -5){
       this.lightSourceSpeed *= -1;
     }
 
+    this.light.position.x += this.lightSourceSpeed * delayN;
     this.renderer.render(this.scene, this.camera);
 
-    this.lastTime = total;
-    requestAnimationFrame(() => this.animate(total));
+    this.lastTime = delay;
+    requestAnimationFrame(() => this.render(delay));
   }
 }
--- src/app/lightning/lightning.component.ts
+++ src/app/lightning/lightning.component.ts
@@ -3,35 +3,33 @@ import {
   AmbientLight,
   AxesHelper, BoxGeometry, Color, DirectionalLight,
   GridHelper,
-  Mesh,
+  Mesh, MeshBasicMaterial,
   MeshPhongMaterial, MeshStandardMaterial,
-  PerspectiveCamera,
+  PerspectiveCamera, PlaneGeometry, PointLight,
   Scene,
-  SphereGeometry,
+  SphereGeometry, SpotLight,
   WebGLRenderer
 } from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 @Component({
-  selector: 'app-lighting',
+  selector: 'lightning',
   templateUrl: './lightning.component.html',
   styleUrls: ['./lightning.component.scss'],
 })
-export class LightningComponent  implements AfterViewInit {
+export class LightningComponent implements AfterViewInit {
   @ViewChild('threeCanvas')
   private canvasRef!: ElementRef;
   private camera!: PerspectiveCamera;
   private scene!: Scene;
   private renderer!: WebGLRenderer;
   private controls!: OrbitControls;
-  private directionalLight!: DirectionalLight;
+  private directionallight!: DirectionalLight;
 
-  async ngAfterViewInit():Promise<void> {
+  async ngAfterViewInit(): Promise<void> {
     this.scene = new Scene();
     this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.camera.position.set(2, 10, 2);
-    this.controls = new OrbitControls( this.camera, this.canvasRef.nativeElement);
-    this.controls.update();
     this.renderer = new WebGLRenderer({canvas: this.canvasRef.nativeElement});
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.shadowMap.enabled = true;
@@ -42,42 +40,55 @@ export class LightningComponent  implements AfterViewInit {
     this.scene.add(gridHelper);
 
 
+    this.controls = new OrbitControls(this.camera, this.canvasRef.nativeElement);
+    this.controls.update();
+
+
     //add box
-    const geometry = new BoxGeometry();
-    const material = new MeshPhongMaterial({color: Color.NAMES.violet});
+    const geometry = new BoxGeometry(1, 1, 1)
+    const material = new MeshStandardMaterial({color: 0x0000ff});
     const box = new Mesh(geometry, material);
-    box.position.set(0, 1, 0);
+    box.position.set(2, 0.5, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
     this.scene.add(box);
 
     //add sphere
-    const geometry2 = new SphereGeometry();
-    const material2 = new MeshPhongMaterial({color: 0xff0000, specular: Color.NAMES.white});
+    const geometry2 = new SphereGeometry(1, 32, 32)
+    const material2 = new MeshPhongMaterial({color: 0xff0000, specular: Color.NAMES.white, shininess: 100 });
+
     const sphere = new Mesh(geometry2, material2);
-    sphere.position.x = -3;
-    sphere.position.y = 2;
+    sphere.position.set(0, 1, 0);
     sphere.castShadow = true;
+    sphere.receiveShadow = true;
     this.scene.add(sphere);
 
+    const sphere2 = new Mesh(geometry2, material2);
+    sphere2.position.set(0, 2.5, 0);
+    sphere2.castShadow = true;
+    sphere2.receiveShadow = true;
+    this.scene.add(sphere2);
 
-    // add plane
-    const planeGeometry = new BoxGeometry(20, 20);
-    const planeMaterial = new MeshStandardMaterial({color: Color.NAMES.white});
-    const plane = new Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = -90 * Math.PI / 180;
-    plane.receiveShadow = true;
-    this.scene.add(plane);
-
-
-    const ambientlight = new AmbientLight(0xffffff, 0.5);
+    //add AmbientLight
+    const ambientlight = new AmbientLight(0xffffff, 0.05);
     this.scene.add(ambientlight);
+    //add DirectionalLight
+    this.directionallight = new DirectionalLight(0xffffff, 1);
+    this.directionallight.position.set(-10, 15, 0);
+    this.directionallight.castShadow = true;
+    this.scene.add(this.directionallight);
 
-    this.directionalLight = new DirectionalLight(0xffffff, 1);
-    this.directionalLight.position.set(-10, 15, 0);
-    this.directionalLight.castShadow = true;
-    this.scene.add(this.directionalLight);
+
+    //add ground plane
+    const groundGeometry = new PlaneGeometry(20, 20);
+    const groundMaterial = new MeshStandardMaterial({color: 0xffffff});
+    const ground = new Mesh(groundGeometry, groundMaterial);
+    ground.position.set(0, 0, 0);
+    ground.rotation.x = -Math.PI / 2;
+    ground.receiveShadow = true;
+    this.scene.add(ground);
 
     requestAnimationFrame(() => this.animate());
-
   }
 
   private animate():void {
