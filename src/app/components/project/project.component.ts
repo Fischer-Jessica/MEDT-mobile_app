a33b69befe6c9e1fd7875f989b281f8270a92259
Fischer <Jessica.Fischer@it.htlhl.at>
2024-01-03 19:17:13 +0100

    adding comments to the steps, so I will remember something about the project in the lesson

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
@@ -8,7 +8,7 @@ import {
   BufferGeometry, WebGLRenderer, MeshStandardMaterial, PlaneGeometry,
   Raycaster, Vector2, AnimationMixer, MeshBasicMaterial, Object3DEventMap
 } from "three";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
 
 @Component({
@@ -16,7 +16,7 @@ import * as THREE from "three";
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
 })
-export class ProjectComponent  implements AfterViewInit {
+export class ProjectComponent implements AfterViewInit {
   @ViewChild('threeCanvas')
   private canvasRef!: ElementRef;
   private camera!: PerspectiveCamera;
@@ -36,24 +36,24 @@ export class ProjectComponent  implements AfterViewInit {
     this.initScene();
     this.addLights();
 
-    //add sphere
-    const geometry2 = new SphereGeometry(1, 32, 32)
+    // Add sphere
+    const geometry2 = new SphereGeometry(1, 32, 32);
     const material2 = new MeshStandardMaterial({color: 0xffff00});
 
     this.sphere = new Mesh(geometry2, material2);
     this.sphere.position.set(-5, 15, 20);
     this.scene.add(this.sphere);
 
-
+    // Load heightmap texture
     const loader = new THREE.TextureLoader();
-    loader.load('assets/project/Heightmap.png', (texture) => this.onTextureLoaded(texture))
+    loader.load('assets/project/Heightmap.png', (texture) => this.onTextureLoaded(texture));
 
-
+    // Create roof
     const roofGeometry = new PlaneGeometry(20, 20);
     const roofMaterial = new MeshPhongMaterial({color: 0xffffff});
     roofMaterial.map = await loader.loadAsync('assets/floor/everytexture.com-stock-wood-texture-00050.jpg');
     roofMaterial.normalMap = await loader.loadAsync('assets/floor/everytexture.com-stock-wood-texture-00050-normal-1024.jpg');
-    roofMaterial.normalScale.set(1, 1); // Effekt der Normalmap verstärken oder abschwächen
+    roofMaterial.normalScale.set(1, 1);
     roofMaterial.side = THREE.DoubleSide;
 
     const roof = new Mesh(roofGeometry, roofMaterial);
@@ -62,12 +62,13 @@ export class ProjectComponent  implements AfterViewInit {
     roof.receiveShadow = true;
     this.scene.add(roof);
 
+    // Load OBJ model (Old_Dusty_Bookshelf)
     const objectLoader = new OBJLoader();
     objectLoader.load('/assets/project/Old_Dusty_Bookshelf.obj', (obj) => {
       const model = obj;
       model.traverse((child) => {
         if (child instanceof THREE.Mesh) {
-          child.material = new MeshStandardMaterial({ color: 0x8B4513})
+          child.material = new MeshStandardMaterial({color: 0x8B4513});
           child.receiveShadow = true;
           child.castShadow = true;
         }
@@ -78,21 +79,19 @@ export class ProjectComponent  implements AfterViewInit {
       model.scale.set(3, 3, 3);
       model.rotation.y = Math.PI;
       this.scene.add(model);
-
     }, (xhr) => {
       console.log((xhr.loaded / xhr.total * 100) + '% loaded');
     }, (error) => {
       console.error('An error happened while loading the model', error);
     });
 
-
-
+    // Load GLTF model (man.gltf)
     const loaderg = new GLTFLoader();
     loaderg.load('/assets/project/man.gltf', (gltf) => {
       const model = gltf.scene;
       model.traverse((child) => {
         if (child instanceof THREE.Mesh) {
-          child.material = new MeshPhongMaterial({ color: 0x8B0000, specular: 0xffffff, shininess: 100 })
+          child.material = new MeshPhongMaterial({color: 0x8B0000, specular: 0xffffff, shininess: 100});
           child.receiveShadow = true;
           child.castShadow = true;
         }
@@ -101,41 +100,40 @@ export class ProjectComponent  implements AfterViewInit {
       model.scale.set(1, 1, 1);
       model.rotation.y = Math.PI / 2;
       this.scene.add(model);
-      // FBX files may contain a mixer with pre-defined animations
 
+      // FBX files may contain a mixer with pre-defined animations
       this.mixer = new THREE.AnimationMixer(model);
 
       // Assuming the first animation is the one you want to play
       const action = this.mixer.clipAction(gltf.animations[0]);
       action.play();
-
     }, (xhr) => {
       console.log((xhr.loaded / xhr.total * 100) + '% loaded');
     }, (error) => {
       console.error('An error happened while loading the model', error);
     });
 
+    // Load another GLTF model (full_man.gltf)
     loaderg.load('/assets/project/full_man.gltf', (gltf) => {
       const model = gltf.scene;
       model.traverse((child) => {
         if (child instanceof THREE.Mesh) {
-          child.material = new MeshPhongMaterial({ color: 0x008B00, specular: 0xffffff, shininess: 100 })
+          child.material = new MeshPhongMaterial({color: 0x008B00, specular: 0xffffff, shininess: 100});
           child.receiveShadow = true;
           child.castShadow = true;
         }
       });
       model.position.set(10, 20, 10);
       model.scale.set(1, 1, 1);
-      model.rotation.y = Math.PI ;
+      model.rotation.y = Math.PI;
       this.scene.add(model);
-      // FBX files may contain a mixer with pre-defined animations
 
+      // FBX files may contain a mixer with pre-defined animations
       this.mixer2 = new THREE.AnimationMixer(model);
 
       // Assuming the first animation is the one you want to play
       const action2 = this.mixer2.clipAction(gltf.animations[0]);
       action2.play();
-
     }, (xhr) => {
       console.log((xhr.loaded / xhr.total * 100) + '% loaded');
     }, (error) => {
@@ -144,9 +142,8 @@ export class ProjectComponent  implements AfterViewInit {
 
     this.addSkybox();
     this.addShelter();
-
-    // add axis, grid helper and camera helper
-    //this.addHelpers();
+    // Add axis, grid helper, and camera helper
+    // this.addHelpers();
 
     this.mousePosition = new Vector2();
 
@@ -178,10 +175,12 @@ export class ProjectComponent  implements AfterViewInit {
     pointlight.position.set(1, 6, 1);
     pointlight.castShadow = true;
     this.scene.add(pointlight);
-    //add AmbientLight
+
+    // Add AmbientLight
     const ambientlight = new AmbientLight(0xffffff, 0.27);
     this.scene.add(ambientlight);
-    //add DirectionalLight
+
+    // Add DirectionalLight
     this.directionallight = new DirectionalLight(0xffffff, 3);
     this.directionallight.position.set(-3, 13, 17);
     this.directionallight.castShadow = true;
@@ -195,7 +194,7 @@ export class ProjectComponent  implements AfterViewInit {
     this.scene.add(this.directionallight);
   }
 
-  private addHelpers(){
+  private addHelpers() {
     const axisHelper = new AxesHelper(6);
     this.scene.add(axisHelper);
     const cameraHelper = new CameraHelper(this.directionallight.shadow.camera);
@@ -214,8 +213,9 @@ export class ProjectComponent  implements AfterViewInit {
     const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
     this.scene.add(skybox);
   }
+
   private addShelter() {
-    //4 Zylinder and a plane as a roof
+    // 4 Cylinders and a plane as a roof
     const geometry = new THREE.CylinderGeometry(0.5, 0.5, 20, 32);
 
     const materialred = new THREE.MeshStandardMaterial({color: 0xff0000});
@@ -228,26 +228,30 @@ export class ProjectComponent  implements AfterViewInit {
     cylinder1.castShadow = true;
     cylinder1.receiveShadow = true;
     this.scene.add(cylinder1);
+
     const cylinder2 = new THREE.Mesh(geometry, materialgreen);
     cylinder2.position.set(2, 10, 16);
     cylinder2.castShadow = true;
     cylinder2.receiveShadow = true;
     this.scene.add(cylinder2);
+
     const cylinder3 = new THREE.Mesh(geometry, materialblue);
     cylinder3.position.set(16, 10, 2);
     cylinder3.castShadow = true;
     cylinder3.receiveShadow = true;
     this.scene.add(cylinder3);
+
     const cylinder4 = new THREE.Mesh(geometry, materialorange);
     cylinder4.position.set(16, 10, 16);
     cylinder4.castShadow = true;
     cylinder4.receiveShadow = true;
     this.scene.add(cylinder4);
   }
+
   private generateTerrain(imagedata: ImageData) {
-    // Generieren des 3D-Geländes basierend auf den Bilddaten der Höhenkarte
-    console.log(`imageData -> width: ${imagedata.width}, height: ${imagedata.height}, data.length: ${imagedata.data.length}`)
-    console.log(imagedata.data)
+    // Generate 3D terrain based on heightmap image data
+    console.log(`imageData -> width: ${imagedata.width}, height: ${imagedata.height}, data.length: ${imagedata.data.length}`);
+    console.log(imagedata.data);
 
     const scale = 1;
 
@@ -259,48 +263,48 @@ export class ProjectComponent  implements AfterViewInit {
       for (let x = 0; x < imagedata.width; x++) {
         const pixelIndex = z * imagedata.width + x;
 
-        // Höhendaten aus den Bilddaten extrahieren und die Y-Koordinate der Vertices festlegen
-        const y = imagedata.data[pixelIndex * 4]
-        vertices.push(x*scale, y * 0.01 * scale, z * scale)
+        // Extract height data from image data and set Y-coordinate of vertices
+        const y = imagedata.data[pixelIndex * 4];
+        vertices.push(x * scale, y * 0.01 * scale, z * scale);
 
-        // Farbdarstellung basierend auf der Höhe
+        // Color representation based on height
         if (y < 128) {
-          colours.push(0.41287, 0.32182, 0.25979, 1)
+          colours.push(0.41287, 0.32182, 0.25979, 1);
         } else if (y > 128 && y < 204.8) {
-          colours.push(0.55153, 0.71476, 0.25920, 1)
+          colours.push(0.55153, 0.71476, 0.25920, 1);
         } else if (y > 204.8) {
-          colours.push(1, 1, 1, 1)
+          colours.push(1, 1, 1, 1);
         }
 
-        // Erzeugung von Dreiecks-Indices für die Darstellung des Terrains
+        // Generate triangle indices for terrain representation
         if (z < imagedata.height - 1 && x < imagedata.width - 1) {
           const topLeft: number = pixelIndex;
           const topRight: number = topLeft + 1;
           const bottomLeft: number = imagedata.width + topLeft;
           const bottomRight: number = bottomLeft + 1;
 
-          indices.push(topLeft, bottomLeft, bottomRight)
-          indices.push(topLeft, bottomRight, topRight)
+          indices.push(topLeft, bottomLeft, bottomRight);
+          indices.push(topLeft, bottomRight, topRight);
         }
       }
     }
 
-    // Erstellen der Geometrie und des Materials für das Gelände
+    // Create geometry and material for the terrain
     const geometry = new THREE.BufferGeometry();
     geometry.setIndex(indices);
     geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
-    geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colours), 4))
-    geometry.scale(0.6, 0.6, 0.6)
+    geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colours), 4));
+    geometry.scale(0.6, 0.6, 0.6);
     const material = new THREE.MeshBasicMaterial();
     material.vertexColors = true;
 
-    // Erstellen des Mesh für das Gelände und Hinzufügen zur Szene
-    this.map = new THREE.Mesh( geometry, material );
-    this.scene.add( this.map );
+    // Create mesh for the terrain and add it to the scene
+    this.map = new THREE.Mesh(geometry, material);
+    this.scene.add(this.map);
   }
 
   private onTextureLoaded(texture: THREE.Texture) {
-    // Wird aufgerufen, wenn die Textur geladen ist. Konvertiert die Textur in Bilddaten und ruft 'generateTerrain' auf
+    // Called when the texture is loaded. Converts the texture to image data and calls 'generateTerrain'
     console.log('texture loaded');
 
     const canvas = document.createElement('canvas');
@@ -327,13 +331,14 @@ export class ProjectComponent  implements AfterViewInit {
     }
     if (this.mixer2) {
       this.mixer2.update(this.clock.getDelta());
-
     }
+
     // Raycaster and intersection logic
+    /* A raycaster is a fundamental concept in computer graphics and computer games. In the context of 3D graphics and interactive applications, a raycaster is an algorithm or component that simulates the behavior of rays in a 3D environment. It is commonly used for tasks like collision detection, picking (selecting) objects in a scene, and rendering realistic lighting effects. */
     this.rayCaster.setFromCamera(this.mousePosition, this.camera);
     const intersections = this.rayCaster.intersectObjects(this.scene.children);
 
-    if(intersections.find(x => x.object.id == this.map.id)){
+    if (intersections.find(x => x.object.id == this.map.id)) {
       this.sphere.material.color.set(0xFF0000);
       this.directionallight.color.set(0xFF0000);
     } else {
